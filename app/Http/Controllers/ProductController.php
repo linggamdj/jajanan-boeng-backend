fec2fa81356a7201d64c213a24b2b5de8a55512f
Lingga <linggaadiatmdj@gmail.com>
2022-07-23 14:04:46 +0700

    style: currency format for product section

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -39,7 +39,7 @@ class ProductController extends Controller
                         </form>';
                 })
                 ->editColumn('price', function ($item) {
-                    return number_format($item->price);
+                    return "Rp".number_format($item->price,2,',','.');
                 })
                 ->rawColumns(['action'])
                 ->make();
